be1564f84bd38d7da35a50d1e6c5ec82d07f0858
Carlos Alcala <alcala21@gmail.com>
2021-12-27 15:50:12 -0600

    Stage 2/4.

--- regression.py
+++ regression.py
@@ -16,17 +16,25 @@ class CustomLinearRegression:
             X = np.column_stack((np.ones_like(y), X))
             b = np.linalg.solve(X.T @ X, X.T @ y)
             self.intercept, self.coefficient = b[0], b[1:]
+        else:
+            self.coefficient = np.linalg.solve(X.T @ X, X.T @ y)
 
-    def print(self):
-        print({'Intercept': self.intercept, 'Coefficient': self.coefficient})
+    def predict(self, X):
+        yhat = X @ self.coefficient
+        if self.fit_intercept:
+            yhat += np.ones_like(yhat) * self.intercept
+        return yhat
 
 
 def run():
-    df = pd.DataFrame({'y': [33, 42, 45, 51, 53, 61, 62],
-                       'x': [4.0, 4.5, 5, 5.5, 6.0, 6.5, 7.0]})
-    lr = CustomLinearRegression()
-    lr.fit(df['x'].values, df['y'])
-    lr.print()
+    df = pd.DataFrame({'x': [4, 4.5, 5, 5.5, 6, 6.5, 7],
+                       'w': [1, -3, 2, 5, 0, 3, 6],
+                       'z': [11, 15, 12, 9, 18, 13, 16],
+                       'y': [33, 42, 45, 51, 53, 61, 62]})
+    X, y = df[['x', 'w', 'z']].values, df['y'].values
+    lr = CustomLinearRegression(fit_intercept=False)
+    lr.fit(X, y)
+    print(lr.predict(X))
 
 
 run()
